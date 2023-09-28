3a3e8b703fbe3bfbeed7cd563e658d55e1827744
AlejoZapataIF <144501421+AlejoZapataIF@users.noreply.github.com>
2023-09-27 20:23:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+// data
+const numeros = [ 2,5,3,4,9,5,67,54 ];
+
+// map() lo que hace es recorrer c/u de los elementos del array
+
+// multipliar cada elmento del array por 10
+const arrayModificado = numeros.map(funcionMultiplicar)
+
+function funcionMultiplicar(numero){
+  return numero * 10;
+}
+document.querySelector('#header').innerHTML = arrayModificado
