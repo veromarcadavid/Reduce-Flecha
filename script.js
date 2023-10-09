d332700c9e71c095eacc7dc27ccfc76f8e2f9c03
Veronica Martinez Cadavid <144962625+veromarcadavid@users.noreply.github.com>
2023-10-09 17:05:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const conjunto = [24,15,4,9,3,7,42 ];
 // map() lo que hace es recorrer c/u de los elementos del array
 
 // multipliar cada elmento del array por 10
-var arrayModificado = multiplicar(conjunto)
+var mult = multiplicar(conjunto)
 
 function multiplicar(arr){
   var total=arr.reduce(function(accumulator, currentElement){
-  return currentElement * 10;
+  return accumulator +(currentElement * 10);
   })
   return total;
  } 
-console.log(arrayModificado);
-//document.getElementById().innerHTML = arrayModificado
+console.log(mult);
+document.write(mult)
